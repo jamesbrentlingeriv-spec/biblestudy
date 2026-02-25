ebb51b68357daa57b5d82843b321fd210da0d51c
jamesbrentlingeriv-spec <jamesbrentlingeriv@gmail.com>
2026-02-24 22:37:45 -0500

    icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bible-study-suite-v2";
+const CACHE_NAME = "bible-study-suite-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -12,19 +12,27 @@ const APP_SHELL = [
   "./icons/icon-32.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icons/apple-touch-icon.png",
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)),
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      ),
   );
   self.clients.claim();
 });
@@ -47,10 +55,12 @@ self.addEventListener("fetch", (event) => {
       return fetch(request)
         .then((response) => {
           const responseToCache = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(request, responseToCache));
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(request, responseToCache));
           return response;
         })
         .catch(() => caches.match("./index.html"));
-    })
+    }),
   );
 });
